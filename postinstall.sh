406b52938eeb7b2024d11067b1c1c2b833cb2aec
SchnuBby2205 <80288097+SchnuBby2205@users.noreply.github.com>
2023-09-05 15:07:37 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -18,5 +18,58 @@ myPrint "green" "*************************\n\n"
 bash -c "sudo sed -i '/\[multilib\]/,/Include/''s/^#//' /etc/pacman.conf"
 bash -c "sudo pacman -Syy"
 
-bash -c "git clone https://github.com/prasanthrangan/hyprdots ~/Hyprdots"
-bash -c "cd ~/Hyprdots"
+printf "\n"
+printRunning "Cloning Hyprdots"
+bash -c "git clone https://github.com/prasanthrangan/hyprdots ~/Hyprdots &>/dev/null"
+cd ~/Hyprdots
+printf "\r"
+printOK "Cloning Hyprdots\n"
+
+myPrint "yellow" "\nUse SchnuBby2205 Configs? (default = y): "
+read CONFIGS
+if [ "${CONFIGS}" == "" ]
+then
+  CONFIGS="y"
+fi
+
+if [ "${CONFIGS}" == "y" ]
+then
+  printf "\n"
+  #sed '/pattern/d' numbers.csv
+  printf "\n"
+  printRunning "Preparing SchnuBby2205 Configs"
+
+  cd Configs/.config/hypr
+  bash -c "sudo rm hyprland.conf &>/dev/null"
+  bash -c "wget https://raw.githubusercontent.com/SchnuBby2205/hyprdots/main/Configs/.config/hypr/hyprland.conf &>/dev/null"
+
+  cd ../waybar/modes
+  bash -c "sudo rm wb_top_01.jsonc &>/dev/null"
+  bash -c "wget https://raw.githubusercontent.com/SchnuBby2205/hyprdots/main/Configs/.config/waybar/modes/wb_top_01.jsonc &>/dev/null"
+
+  cd ~/Hyprdots/Scripts
+  bash -c "sudo rm custom_apps.lst &>/dev/null"
+  bash -c "wget https://raw.githubusercontent.com/SchnuBby2205/hyprdots/main/Scripts/custom_apps.lst &>/dev/null"
+  bash -c "sed '/bluez/d' custom_hypr.lst"
+  bash -c "sed '/bluez-utils/d' custom_hypr.lst"
+  bash -c "sed '/blueman/d' custom_hypr.lst"
+  bash -c "sed '/brightnessctl/d' custom_hypr.lst"  
+  
+  printf "\r"
+  printOK "Preparing SchnuBby2205 Configs\n"
+fi
+
+printf "\nPostinstallation will start in 5 seconds."
+sleep 1 
+printf "\rPostinstallation will start in 4 seconds."
+sleep 1 
+printf "\rPostinstallation will start in 3 seconds."
+sleep 1 
+printf "\rPostinstallation will start in 2 seconds."
+sleep 1 
+printf "\rPostinstallation will start in 1 seconds."
+sleep 1 
+clearScreen
+
+cd ~/Hyprdots/Scripts
+bash -c "./install.sh custom_apps.lst"
