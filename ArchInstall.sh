fae3e87aa1d8499e9ab71dd54c5b915dccd88aa9
SchnuBby2205 <80288097+SchnuBby2205@users.noreply.github.com>
2024-10-29 15:38:16 +0100

    Update ArchInstall.sh

--- ArchInstall.sh
+++ ArchInstall.sh
@@ -200,21 +200,10 @@ then
 	#bash -c "sed -i '/ParallelDownloads/s/^#//' /etc/pacman.conf"
 
 	printf "\r"
-	printOK "Setting up pacman      \n"
+	printOK "Setting up pacman      \n\n"
 	#-----------------------------------------------------------
 
-	clearScreen
-	myPrint "green" "    ____           __        _____            \n"
-	myPrint "green" "   /  _/___  _____/ /_____ _/ / (_)___  ____ _\n"
-	myPrint "green" "   / // __ \/ ___/ __/ __ \`/ / / / __ \/ __ \`/\n"
-	myPrint "green" " _/ // / / (__  ) /_/ /_/ / / / / / / / /_/ / \n"
-	myPrint "green" "/___/_/ /_/____/\__/\__,_/_/_/_/_/ /_/\__, /  \n"
-	myPrint "green" "   /   |  __________/ /_             /____/   \n"
-	myPrint "green" "  / /| | / ___/ ___/ __ \                     \n"
-	myPrint "green" " / ___ |/ /  / /__/ / / /                     \n"
-	myPrint "green" "/_/  |_/_/   \___/_/ /_/                      \n\n"
-	
-	myPrint "green" "Starting installation"
+ 	myPrint "green" "Starting installation"
 	#bash -c "archinstall --conf https://raw.githubusercontent.com/SchnuBby2205/ArchInstall/main/conf.json --creds https://raw.githubusercontent.com/SchnuBby2205/ArchInstall/main/creds.json"
 	#bash -c "cp ./ArchInstall.sh /mnt/home/schnubby/ &>/dev/null"
 fi
