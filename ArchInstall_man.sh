da35ad595878054cf3a7e636089135c2c91cd869
SchnuBby2205 <80288097+SchnuBby2205@users.noreply.github.com>
2024-11-06 14:39:21 +0100

    Update ArchInstall_man.sh

--- ArchInstall_man.sh
+++ ArchInstall_man.sh
@@ -242,12 +242,12 @@ then
 	printRunning "Running base install..."
 	bash -c "pacstrap -K /mnt base base-devel linux-lts linux-firmware intel-ucode efibootmgr grub sudo git networkmanager lutris &>/dev/null"
   	bash -c "genfstab -U /mnt >> /mnt/etc/fstab"
-   	bash -c "cp ${FILENAME} /mnt"
+   	bash -c "cp ./${FILENAME} /mnt"
 	#myPrint "green" "\n\nRun ./ArchInstall option 2\n\n"
 	printf "\r"
 	printOK "Running base install...\n"
  	#---------------Running base install---------------
-   	bash -c "arch-chroot /mnt ${FILENAME} 2 2 3 4 5 6 ${HOSTNAME} ${USER}"
+   	bash -c "arch-chroot /mnt ./${FILENAME} 2 2 3 4 5 6 ${HOSTNAME} ${USER}"
     	bash -c "umount -R /mnt &>/dev/null"
 
   	myPrint "green" "\nInstallation complete! Restart in 3..."
@@ -461,8 +461,8 @@ then
 	printOK "Enabling services...\n"
 	#---------------Enabling services---------------
 
-   	bash -c "mv ${FILENAME} /home/schnubby/"
-    	bash -c "echo ${FILENAME} 3 >> /home/schnubby/.bashrc"
+   	bash -c "mv ./${FILENAME} /home/schnubby/"
+    	bash -c "echo ./${FILENAME} 3 >> /home/schnubby/.bashrc"
  	#myPrint "green" "\n\nInstallation complete! run exit, umount -R /mnt then reboot!\n\n"
 fi    
 
