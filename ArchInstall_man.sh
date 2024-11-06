9eeeaf147209ba403ed453306d59af94dc1678cd
SchnuBby2205 <80288097+SchnuBby2205@users.noreply.github.com>
2024-11-06 14:26:11 +0100

    Update ArchInstall_man.sh

--- ArchInstall_man.sh
+++ ArchInstall_man.sh
@@ -352,12 +352,12 @@ then
  	#---------------Installing Config files---------------
 
  	#---------------Installing gaming dependencies---------------
-	printRunning "Installing gaming dependencies..."
- 	bash -c "yay arch gaming meta &>/dev/null"
- 	bash -c "yay -S dxvk-bin &>/dev/null"
+ 	bash -c "yay arch gaming meta"
+ 	bash -c "yay -S dxvk-bin"
   	#bash -c "yay -S wine-ge-custom"
 	bash -c "sudo pacman -Syu &>/dev/null"
 	bash -c "sudo pacman -S wine-staging &>/dev/null"
+	printRunning "Installing gaming dependencies..."
 	bash -c "sudo pacman -S --needed --asdeps giflib lib32-giflib gnutls lib32-gnutls v4l-utils lib32-v4l-utils libpulse \
 	lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib sqlite lib32-sqlite libxcomposite \
 	lib32-libxcomposite ocl-icd lib32-ocl-icd libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs \
