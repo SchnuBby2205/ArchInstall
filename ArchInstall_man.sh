74b136103f7bb7dfcdae89601228d61e320a8b2d
SchnuBby2205 <80288097+SchnuBby2205@users.noreply.github.com>
2024-11-06 15:16:19 +0100

    Update ArchInstall_man.sh

--- ArchInstall_man.sh
+++ ArchInstall_man.sh
@@ -4,7 +4,7 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
-WHITE='\033[0;37m'
+WHITE='\033[0;97m'
 NC='\033[0m'
 RUNNING="[${YELLOW}  RUNNING ${NC}]"
 MYOK="[${GREEN}    OK    ${NC}]"
