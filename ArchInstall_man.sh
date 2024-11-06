17bfcaef10f4ff610f6240ef2c0c941dbbedab14
SchnuBby2205 <80288097+SchnuBby2205@users.noreply.github.com>
2024-11-06 09:25:11 +0100

    Prints wieder einbauen

--- ArchInstall_man.sh
+++ ArchInstall_man.sh
@@ -5,6 +5,8 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
+RUNNING="[${YELLOW}  RUNNING ${NC}]"
+MYOK="[${GREEN}    OK    ${NC}]"
 ERROR="[${RED}  ERROR   ${NC}]"
 
 # Partitionen
@@ -34,6 +36,16 @@ myPrint() {
 	then
 		printf "${YELLOW}${message}${NC}"
 	fi
+}
+
+printRunning() {
+	message="$1"
+	printf "${RUNNING}   ${message}"
+}
+
+printError() {
+	message="$1"
+	printf "${ERROR}   ${message}"
 }
 
 printError() {
