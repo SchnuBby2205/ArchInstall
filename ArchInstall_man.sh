ed0e2fe1739aac3ada8ef0d4eab6d2380ea2468d
SchnuBby2205 <80288097+SchnuBby2205@users.noreply.github.com>
2024-11-11 07:22:51 +0100

    Update ArchInstall_man.sh

--- ArchInstall_man.sh
+++ ArchInstall_man.sh
@@ -429,17 +429,19 @@ then
  	#---------------Installing Config files---------------
 
  	#---------------Installing gaming dependencies---------------
- 	bash -c "yay arch gaming meta"
- 	bash -c "yay -S dxvk-bin"
+ 	#bash -c "yay arch gaming meta"
+  	bash -c 'echo y | LANG=C yay --noprovides --answerdiff None --answerclean None --mflags "--noconfirm" arch gaming meta'
+ 	#bash -c "yay -S dxvk-bin"
+  	bash -c 'echo y | LANG=C yay --noprovides --answerdiff None --answerclean None --mflags "--noconfirm" dxvk-bin'
   	#bash -c "yay -S wine-ge-custom"
 	bash -c "sudo pacman -Syu &>/dev/null"
-	bash -c "sudo pacman -S wine-staging &>/dev/null"
+	bash -c "sudo pacman --noconfirm -S wine-staging &>/dev/null"
 	printRunning "Installing ${WHITE}gaming dependencies...${NC}"
-	bash -c "sudo pacman -S --needed --asdeps giflib lib32-giflib gnutls lib32-gnutls v4l-utils lib32-v4l-utils libpulse \
+	bash -c "sudo pacman --noconfirm -S --needed --asdeps giflib lib32-giflib gnutls lib32-gnutls v4l-utils lib32-v4l-utils libpulse \
 	lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib sqlite lib32-sqlite libxcomposite \
 	lib32-libxcomposite ocl-icd lib32-ocl-icd libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs \
 	lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader sdl2 lib32-sdl2 lib32-gamemode &>/dev/null"
- 	bash -c "sudo pacman -S --needed lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader &>/dev/null"
+ 	bash -c "sudo pacman --noconfirm -S --needed lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader &>/dev/null"
 	printf "\r"
 	printOK "Installing ${WHITE}gaming dependencies...${NC}\n"
  	#---------------Installing gaming dependencies---------------
