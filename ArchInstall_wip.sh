df7a92611e2946641b915b260f73eee2b58ac8c8
SchnuBby2205 <80288097+SchnuBby2205@users.noreply.github.com>
2025-08-28 11:47:02 +0200

    Moved defaults to the start of the File

--- ArchInstall_wip.sh
+++ ArchInstall_wip.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+if [[ -n "$defaults" && "$option" -eq 1 ]]; then
+	boot="/dev/nvme0n1p1"
+	swap="/dev/nvme0n1p2"
+	root="/dev/nvme0n1p3"
+	hostname="ArchLinux"
+	user="schnubby"
+ 	desktop="caelestia"
+	installBaseSystem
+fi
+
 # Farben im Terminal
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -538,15 +549,6 @@ if [[ "$debug" =~ ^[yY]$ ]]; then
 	debugstring=""
 else
 	debugstring=" &>/dev/null"
-fi
-if [[ -n "$defaults" && "$option" -eq 1 ]]; then
-	boot="/dev/nvme0n1p1"
-	swap="/dev/nvme0n1p2"
-	root="/dev/nvme0n1p3"
-	hostname="ArchLinux"
-	user="schnubby"
- 	desktop="caelestia"
-	installBaseSystem
 fi
 if [[ -z "$option" ]]; then
 	listOptions
